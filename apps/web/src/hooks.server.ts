1354a829f2e57e8dded49f47a6b48200c544ff08
William Fairclotb <willuhmjs@gmail.com>
2024-01-03 14:02:32 +0000

    chore: delete templates and lint

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,18 +1,19 @@
-import detectObjects from "$lib/server/model";
+import detectObjects from '$lib/server/model';
 import http from 'http';
 
 http.get('http://localhost:8080/stream', (res) => {
-  let imageBuffer = Buffer.alloc(0);
+	let imageBuffer = Buffer.alloc(0);
 
-  res.on('data', async (chunk) => {
-    imageBuffer = Buffer.concat([imageBuffer, chunk]);
-    const start = imageBuffer.indexOf(Buffer.from([0xFF, 0xD8]));
-    const end = imageBuffer.indexOf(Buffer.from([0xFF, 0xD9]));
+	res.on('data', async (chunk) => {
+		imageBuffer = Buffer.concat([imageBuffer, chunk]);
+		const start = imageBuffer.indexOf(Buffer.from([0xff, 0xd8]));
+		const end = imageBuffer.indexOf(Buffer.from([0xff, 0xd9]));
 
-    if (start !== -1 && end !== -1) {
-        const image = imageBuffer.subarray(start, end + 2); imageBuffer = imageBuffer.subarray(end + 2);
-        const detection = await detectObjects(image);
-        console.log(detection)    
-    }
-  });
+		if (start !== -1 && end !== -1) {
+			const image = imageBuffer.subarray(start, end + 2);
+			imageBuffer = imageBuffer.subarray(end + 2);
+			const detection = await detectObjects(image);
+			console.log(detection);
+		}
+	});
 });
