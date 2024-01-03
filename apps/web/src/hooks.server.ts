742e6c2c79105279bf93877e7877ef6ac913f886
William Fairclotb <willuhmjs@gmail.com>
2024-01-03 13:56:12 +0000

    feat: add mocking

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,14 +1,11 @@
 import detectObjects from "$lib/server/model";
-
-// TODO impl; detectObjects(Buffer)
 import http from 'http';
 
-http.get('http://192.168.50.236:8080/stream', (res) => {
+http.get('http://localhost:8080/stream', (res) => {
   let imageBuffer = Buffer.alloc(0);
 
   res.on('data', async (chunk) => {
     imageBuffer = Buffer.concat([imageBuffer, chunk]);
-
     const start = imageBuffer.indexOf(Buffer.from([0xFF, 0xD8]));
     const end = imageBuffer.indexOf(Buffer.from([0xFF, 0xD9]));
 
