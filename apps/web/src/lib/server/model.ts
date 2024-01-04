a1f6150b2b1443b5ab000265e379f8cde8cc26f3
William Faircloth <willuhmjs@gmail.com>
2024-01-04 12:19:13 -0800

    feat: use object, prob as percent

--- apps/web/src/lib/server/model.ts
+++ apps/web/src/lib/server/model.ts
@@ -2,7 +2,13 @@ import ort from 'onnxruntime-node';
 import sharp from 'sharp';
 import { writable } from 'svelte/store';
 
-type Box = [number, number, number, number, string, number];
+interface Box {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    prob: number;
+}
 
 export const latestDetection = writable<Box[]>();
 
@@ -50,57 +56,52 @@ async function runModel(modelUrl: string, input: number[]) {
 }
 
 // Converts YOLOv8 output to array of detected objects
-function processOutput(output, imgWidth: number, imgHeight: number) {
-	let boxes: Box[] = [];
-	for (let index = 0; index < 8400; index++) {
-		const [classId, prob] = [...Array(80).keys()]
-			.map((col) => [col, output[8400 * (col + 4) + index]])
-			.reduce((accumulator, item) => (item[1] > accumulator[1] ? item : accumulator), [0, 0]);
+function processOutput(output: any[], imgWidth: number, imgHeight: number) {
+    let boxes: Box[] = [];
+    for (let index = 0; index < 8400; index++) {
+        const [classId, prob] = [...Array(80).keys()]
+            .map((col) => [col, output[8400 * (col + 4) + index]])
+            .reduce((accumulator, item) => (item[1] > accumulator[1] ? item : accumulator), [0, 0]);
 
-		if (prob < 0.5) continue;
+        if (prob < 0.5) continue;
 
-		const label = 'spaghetti';
-		const xc = output[index];
-		const yc = output[8400 + index];
-		const w = output[2 * 8400 + index];
-		const h = output[3 * 8400 + index];
-		const x1 = ((xc - w / 2) / 640) * imgWidth;
-		const y1 = ((yc - h / 2) / 640) * imgHeight;
-		const x2 = ((xc + w / 2) / 640) * imgWidth;
-		const y2 = ((yc + h / 2) / 640) * imgHeight;
-		boxes.push([x1, y1, x2, y2, label, prob]);
-	}
+        const xc = output[index];
+        const yc = output[8400 + index];
+        const w = output[2 * 8400 + index];
+        const h = output[3 * 8400 + index];
+        const x1 = ((xc - w / 2) / 640) * imgWidth;
+        const y1 = ((yc - h / 2) / 640) * imgHeight;
+        const x2 = ((xc + w / 2) / 640) * imgWidth;
+        const y2 = ((yc + h / 2) / 640) * imgHeight;
+        boxes.push({x1, y1, x2, y2, prob: Math.round(prob * 100) });
+    }
 
-	boxes = boxes.sort((box1, box2) => box2[5] - box1[5]);
-	const result: Box[] = [];
-	while (boxes.length > 0) {
-		result.push(boxes[0]);
-		boxes = boxes.filter((box) => iou(boxes[0], box) < 0.7);
-	}
-	return result;
+    boxes = boxes.sort((box1, box2) => box2.prob - box1.prob);
+    const result: Box[] = [];
+    while (boxes.length > 0) {
+        result.push(boxes[0]);
+        boxes = boxes.filter((box) => iou(boxes[0], box) < 0.7);
+    }
+    return result;
 }
 
 // Calculates Intersection-over-union (IoU) for two boxes
 function iou(box1: Box, box2: Box) {
-	return intersection(box1, box2) / union(box1, box2);
+    return intersection(box1, box2) / union(box1, box2);
 }
 
 // Calculates union area of two boxes
 function union(box1: Box, box2: Box) {
-	const [box1X1, box1Y1, box1X2, box1Y2] = box1;
-	const [box2X1, box2Y1, box2X2, box2Y2] = box2;
-	const box1Area = (box1X2 - box1X1) * (box1Y2 - box1Y1);
-	const box2Area = (box2X2 - box2X1) * (box2Y2 - box2Y1);
-	return box1Area + box2Area - intersection(box1, box2);
+    const box1Area = (box1.x2 - box1.x1) * (box1.y2 - box1.y1);
+    const box2Area = (box2.x2 - box2.x1) * (box2.y2 - box2.y1);
+    return box1Area + box2Area - intersection(box1, box2);
 }
 
 // Calculates intersection area of two boxes
 function intersection(box1: Box, box2: Box) {
-	const [box1X1, box1Y1, box1X2, box1Y2] = box1;
-	const [box2X1, box2Y1, box2X2, box2Y2] = box2;
-	const x1 = Math.max(box1X1, box2X1);
-	const y1 = Math.max(box1Y1, box2Y1);
-	const x2 = Math.min(box1X2, box2X2);
-	const y2 = Math.min(box1Y2, box2Y2);
-	return (x2 - x1) * (y2 - y1);
+    const x1 = Math.max(box1.x1, box2.x1);
+    const y1 = Math.max(box1.y1, box2.y1);
+    const x2 = Math.min(box1.x2, box2.x2);
+    const y2 = Math.min(box1.y2, box2.y2);
+    return (x2 - x1) * (y2 - y1);
 }
