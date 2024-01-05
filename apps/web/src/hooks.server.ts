0d3daba21e36ba0786f01b5b8d34bd7b38ef7fd5
William Faircloth <willuhmjs@gmail.com>
2024-01-04 19:50:41 -0800

    fix: quality improvements

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,26 +1,32 @@
+import axios from 'axios';
+import MjpegConsumer from 'mjpeg-consumer';
+
 import server from "$lib/server/ws";
 import { detectObjects, latestDetection } from '$lib/server/model';
-import http from 'http';
 
-http.get('http://localhost:8080/stream', (res) => {
-	let imageBuffer = Buffer.alloc(0);
-
-	res.on('data', async (chunk) => {
-		imageBuffer = Buffer.concat([imageBuffer, chunk]);
-		const start = imageBuffer.indexOf(Buffer.from([0xff, 0xd8]));
-		const end = imageBuffer.indexOf(Buffer.from([0xff, 0xd9]));
-
-		if (start !== -1 && end !== -1) {
-			const image = imageBuffer.subarray(start, end + 2);
-			imageBuffer = imageBuffer.subarray(end + 2);
-			await detectObjects(image);
-		}
-	});
-});
-
+let counter = 0;
 
 server.on("connection", socket => {
-	latestDetection.subscribe((val) => {
-		socket.send(JSON.stringify(val));
-	})
-})
+    latestDetection.subscribe((val) => {
+        socket.send(JSON.stringify(val));
+    });
+
+    const mjpegConsumer = new MjpegConsumer();
+
+    const requestConfig = {
+        url: 'http://localhost:8080/stream',
+        responseType: 'stream',
+    };
+
+    axios(requestConfig).then(response => {
+        response.data.pipe(mjpegConsumer)
+            .on('data', async (frame: Buffer) => {
+                counter++;
+                if (counter % 2 === 0) {
+                    await detectObjects(frame);
+                }
+            });
+    }).catch(error => {
+        console.error(error);
+    });
+});
