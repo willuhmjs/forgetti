8312773f32b81d888856a885eb447181f07d4d26
willuhmjs <willuhmjs@gmail.com>
2024-01-19 17:27:02 -0500

    chore: use config.example.json

--- src/lib/configStore.ts
+++ src/lib/configStore.ts
@@ -1,5 +1,9 @@
 import { writable } from 'svelte/store';
-import currentConfig from '../../config.json?raw';
+import fs from "fs";
+import configExample from '../../config.example.json?raw';
+
+if (!fs.existsSync("./config.json")) fs.writeFileSync("./config.json", configExample, "utf-8")
+const currentConfig = fs.readFileSync("./config.json", "utf-8");
 
 interface Config {
     CameraURL: string
