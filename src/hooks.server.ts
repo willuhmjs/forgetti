245baba2fca65d7d6da74a83a94d41096d393dcc
William Faircloth <willuhmjs@gmail.com>
2024-01-18 20:13:43 -0500

    chore: move ws out of config subscribe

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,42 +4,43 @@ import configStore from "$lib/configStore";
 import server from "$lib/server/ws";
 import { detectObjects, latestDetection } from '$lib/server/model';
 
-configStore.subscribe((config) => {
-    server.on("connection", socket => {
-        latestDetection.subscribe((val) => {
-            socket.send(JSON.stringify(val));
-        });
-
-        const mjpegConsumer = new MjpegConsumer();
-
-        const requestConfig: AxiosRequestConfig = {
-            url: config['CameraURL'],
-            responseType: 'stream',
-        };
-
-        let processing = false;
-        const process = async (frameBuffer: Buffer) => {
-            processing = true;
-            await detectObjects(frameBuffer);
-            processing = false;
-        }
-
-        axios(requestConfig).then(response => {
-            const stream = response.data.pipe(mjpegConsumer);
-
-            stream.on('data', (frame: Buffer) => {
-                if (frame && !processing) {
-                    try {
-                        process(frame);
-                    } catch (e) {
-                        console.error(e);
-                    }
-                }
-            });
-
-                
-        }).catch(error => {
-            console.error(error);
-        }); 
+server.on("connection", socket => {
+    latestDetection.subscribe((val) => {
+        socket.send(JSON.stringify(val));
     });
 })
+
+
+configStore.subscribe((config) => {
+    const mjpegConsumer = new MjpegConsumer();
+
+    const requestConfig: AxiosRequestConfig = {
+        url: config['CameraURL'],
+        responseType: 'stream',
+    };
+
+    let processing = false;
+    const process = async (frameBuffer: Buffer) => {
+        processing = true;
+        await detectObjects(frameBuffer);
+        processing = false;
+    }
+
+    axios(requestConfig).then(response => {
+        const stream = response.data.pipe(mjpegConsumer);
+
+        stream.on('data', (frame: Buffer) => {
+            if (frame && !processing) {
+                try {
+                    process(frame);
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        });
+
+            
+    }).catch(error => {
+        console.error(error);
+    }); 
+});
