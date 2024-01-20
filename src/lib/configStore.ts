dc39550e78f7ee92dff967019c2edd9f03d49826
William Faircloth <willuhmjs@gmail.com>
2024-01-20 09:24:59 -0800

    refactor: define Config interface globally

--- src/lib/configStore.ts
+++ src/lib/configStore.ts
@@ -1,13 +1,11 @@
 import { writable } from 'svelte/store';
 import fs from "fs";
+import type { Config } from "$lib/types";
 import configExample from '../../config.example.json?raw';
+
 
 if (!fs.existsSync("./config.json")) fs.writeFileSync("./config.json", configExample, "utf-8")
 const currentConfig = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
-interface Config {
-    Enabled: boolean
-    CameraURL: string,
-}
 
 export default writable<Config>(currentConfig);
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,3 +10,9 @@ export interface InferenceData {
 	box: Box[];
 	buffer: string;
 }
+
+
+export interface Config {
+    Enabled: boolean
+    CameraURL: string,
+}
