0683681c12d8328f7b96ab7b3f358dcac507c435
William Faircloth <willuhmjs@gmail.com>
2024-01-20 20:37:56 -0800

    feat: grid system for windows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,30 @@
-<script lang="ts">
-	import Config from '$lib/components/Config.svelte';
+<script>
+    import Config from '$lib/components/Config.svelte';
     import LivePreview from '$lib/components/LivePreview.svelte';
-	import Window from '$lib/components/Window.svelte';
-	import { faCog, faVideoCamera } from '@fortawesome/free-solid-svg-icons';
+    import Window from '$lib/components/Window.svelte';
+    import { faCog, faVideoCamera } from '@fortawesome/free-solid-svg-icons';
     export let data;
-
-   
 </script>
 
-<Window title="Camera" icon={faVideoCamera}>
-    <LivePreview />
-</Window>
+<style>
+    .window-container {
+        display: flex;
+        gap: 10px;
+        flex-wrap: wrap;
+        align-items: flex-start; 
+    }
+</style>
 
-<Window title="Configuration" icon={faCog}>
-    <Config {data} />
-</Window>
+<div class="desktop">
+    <div class="window-container">
+        <Window title="Camera" icon={faVideoCamera}>
+            <LivePreview />
+        </Window>
 
+        <Window title="Configuration" icon={faCog}>
+            <Config {data} />
+        </Window>
+        
+        
+    </div>
+</div>
