b698789623f7f96c668d5ac2649f5608d43e1c7a
William Faircloth <willuhmjs@gmail.com>
2024-01-26 23:59:23 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import "sharp";
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	assetsInclude: ['**/*.onnx', '**/*.json'],
-	optimizeDeps: {
-		exclude: ['sharp']
-	}
 });
