4cfffd2777d71b2dedb036441a3cef12aa337df3
William Faircloth <willuhmjs@gmail.com>
2024-01-29 19:51:51 -0500

    fix: handle log data correctly

--- src/lib/components/Logs.svelte
+++ src/lib/components/Logs.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
     import logsStore from "$lib/logsStore";
-    import type { AppUpdateResponsePacket } from "$lib/types";
-	import { get } from "svelte/store";
-
-    let logs: AppUpdateResponsePacket[] = get(logsStore);
-    logsStore.subscribe((value) => {
-        logs = value;
-   });
 </script>
 
 <div class="appUpdate">
-    {#each logs as update}
+    {#each $logsStore as update}
         <p class="update {update.type}">
             <span>{update.command}: {update.message}</span>
             <span>{new Date().toLocaleTimeString()}</span>
