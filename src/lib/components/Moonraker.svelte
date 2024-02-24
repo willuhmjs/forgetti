9ccf1da298c8bed1454a7788c188785c2c52d75d
William Faircloth <willuhmjs@gmail.com>
2024-02-24 14:20:12 -0500

    fix: destroy interval as needed

--- src/lib/components/Moonraker.svelte
+++ src/lib/components/Moonraker.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
+    let intervalId: string | number | NodeJS.Timeout | null | undefined = null;
+
     import type { Config } from "$lib/types";
+	import { onDestroy, onMount } from "svelte";
     export let liveData: Config;
-    $: moonrakerEnabled = liveData.MoonrakerEnabled;
-    setInterval(async () => {
-        console.log(moonrakerEnabled);
-        if (!liveData.MoonrakerURL || !liveData.MoonrakerEnabled) return;
-        const url = new URL("/printer/objects/query?print_stats", liveData.MoonrakerURL);
-        try {
-            const response = await fetch(url.href);
-            const json = await response.json();
-        } catch (e) {
+   
+    onMount(() => {
+        intervalId = setInterval(async () => {
+            console.log(liveData.MoonrakerEnabled)
+            if (!liveData.MoonrakerURL || !liveData.MoonrakerEnabled) return;
+            const url = new URL("/printer/objects/query?print_stats", liveData.MoonrakerURL);
+            try {
+                const response = await fetch(url.href);
+                const json = await response.json();
+            } catch (e) {
+            }
+        }, 1000);
+    });
+
+    onDestroy(() => {
+        if (intervalId) {
+            clearInterval(intervalId);
         }
-    }, 1000)
+    });
 </script>
-{moonrakerEnabled}
+{liveData.MoonrakerEnabled}
 {Date.now()}
