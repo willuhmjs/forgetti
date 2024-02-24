3f6634930a1061c94530635b187507c8e9070193
William Faircloth <willuhmjs@gmail.com>
2024-02-24 17:33:49 -0500

    feat: more info, style moonraker box

--- src/lib/components/Moonraker.svelte
+++ src/lib/components/Moonraker.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	type interval = NodeJS.Timeout | string | number | undefined;
 	import Fa from 'svelte-fa';
-	import { faHourglassHalf, faRuler, faSpinner, faSync } from '@fortawesome/free-solid-svg-icons';
+	import { faHourglassHalf, faRuler, faSpinner, faSync, faCube, faIndustry } from '@fortawesome/free-solid-svg-icons';
 	interface printStats {
 		filename: string;
 		total_duration: number;
@@ -33,22 +33,67 @@
 	});
 </script>
 
-<div>
+<div class="moonrakerContainer">
+    <div>
 	{#if latestStats}
-		{#if latestStats.state === 'printing'}
-			<p>
-				<Fa icon={faSpinner} spin />
-				{latestStats.filename}
-			</p>
-			<p>
-				<Fa icon={faRuler} />
-				{(latestStats.filament_used / 1000).toFixed(2)}m
-			</p>
-		{:else if latestStats.state === 'standby'}
-			<Fa icon={faHourglassHalf} />
-			{latestStats.state}
+		{#if latestStats.state !== 'standby'}
+            <p class="spec">
+                <span class="icon">
+                    <Fa icon={faCube} />
+                </span>
+                {latestStats.filename}
+            </p>
+            <p class="spec">
+                <span class="icon">
+                    {#if latestStats.state == 'printing'}
+                        <Fa icon={faSpinner} spin />
+                    {:else if latestStats.state == 'paused'}
+                        <Fa icon={faHourglassHalf} />
+                    {:else}
+                        <Fa icon={faIndustry} />
+                    {/if}
+                </span>
+                {latestStats.state.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')}
+            </p>
+            <p class="spec">
+                <span class="icon">
+                    <Fa icon={faRuler} />
+                </span>
+                {(latestStats.filament_used / 1000).toFixed(2)}m
+            </p>
+		{:else}
+        <p class="spec">
+            <span class="icon">
+                <Fa icon={faHourglassHalf} />
+            </span>
+            {latestStats.state}
+        </p>
 		{/if}
 	{:else}
 		<Fa icon={faSync} spin />
 	{/if}
+    </div>
 </div>
+
+<style>
+    .spec {
+		font-size: 0.9rem;
+	}
+
+	.spec .icon:first-of-type {
+		margin-right: 0.5rem;
+	}
+
+	.spec .icon {
+		color: var(--brand);
+	}
+
+	.moonrakerContainer {
+		display: flex;
+		padding: 0 1rem;
+		gap: 1.5rem;
+		justify-content: space-between;
+		align-items: middle;
+	}
+
+</style>
